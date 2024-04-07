ed254970e903f515ed5a519b9cbe6edd47f2d09f
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-04-07 17:10:29 +0300

    Update server and client to display standard message

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = 8080;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 
--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
+
+async function fetchData(url, property) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data[property];
+}
 
 function App() {
+    const [message, setMessage] = useState('');
+
+    useLayoutEffect(() => {
+        async function fetchMessage() {
+            const message = await fetchData('/api', 'message');
+            setMessage(message);
+        }
+        fetchMessage();
+    }, []);
+
     return (
-        <div>App</div>
+        <div>
+            {(typeof (message) === "undefined") ? (
+                <p>Loading...</p>
+            ) : (
+                <p>{message}</p>
+            )}
+        </div>
     )
 }
 
