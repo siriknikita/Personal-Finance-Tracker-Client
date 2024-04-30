fe83f7e23d2546f0d51f3e77d4c6c01b31b50c0b
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-04-30 21:29:46 +0300

    Fix minor bug with date in goal form

--- src/scenes/forms/goalForm/index.jsx
+++ src/scenes/forms/goalForm/index.jsx
@@ -8,74 +8,70 @@ import Sidebar from "../../global/Sidebar";
 import Topbar from "../../global/Topbar";
 
 function GoalForm() {
-    const [theme, colorMode] = useMode();
-    const [isSidebar, setIsSidebar] = useState(true);
-    const { user } = useContext(UserContext);
-    const [goalDescripiton, setGoalDescription] = useState("");
-    const [deadline, setDeadline] = useState("");
-    const navigate = useNavigate();
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+  const { user } = useContext(UserContext);
+  const [goalDescripiton, setGoalDescription] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await fetch(`/api/set/goal`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    userID: user.userID,
-                    goal: goalDescripiton,
-                    deadline: deadline,
-                }),
-            });
-            navigate("/goals");
-            toast.success("Goal added successfully!");
-        } catch (error) {
-            console.error(error);
-            toast.error("Failed to add goal!");
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await fetch(`/api/set/goal`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userID: user.userID,
+          goal: goalDescripiton,
+          deadline: deadline,
+        }),
+      });
+      navigate("/goals");
+      toast.success("Goal added successfully!");
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to add goal!");
+    }
+  };
 
-    return (
-        <ColorModeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <div className="app">
-                    <Sidebar isSidebar={isSidebar} user={user} />
-                    <main className="content">
-                        <Topbar
-                            isSidebar={isSidebar}
-                            setIsSidebar={setIsSidebar}
-                        />
-                        <h3>Set your goals!</h3>
-                        <form onSubmit={handleSubmit}>
-                            <label>Goal:</label>
-                            <br />
-                            <input
-                                type="text"
-                                value={goalDescripiton}
-                                placeholder="Your goal description"
-                                onChange={(e) =>
-                                    setGoalDescription(e.target.value)
-                                }
-                            />
-                            <br />
-                            <label>Deadline:</label>
-                            <br />
-                            <input
-                                type="date"
-                                value={deadline}
-                                onChange={(e) => setDeadline(e.target.value)}
-                            />
-                            <br />
-                            <button>Submit</button>
-                        </form>
-                    </main>
-                </div>
-            </ThemeProvider>
-        </ColorModeContext.Provider>
-    );
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} user={user} />
+          <main className="content">
+            <Topbar isSidebar={isSidebar} setIsSidebar={setIsSidebar} />
+            <h3>Set your goals!</h3>
+            <form onSubmit={handleSubmit}>
+              <label>Goal:</label>
+              <br />
+              <input
+                type="text"
+                value={goalDescripiton}
+                placeholder="Your goal description"
+                onChange={(e) => setGoalDescription(e.target.value)}
+              />
+              <br />
+              <label>Deadline:</label>
+              <br />
+              <input
+                type="date"
+                value={deadline}
+                min={new Date().toISOString().split("T")[0]}
+                onChange={(e) => setDeadline(e.target.value)}
+              />
+              <br />
+              <button>Submit</button>
+            </form>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default GoalForm;
