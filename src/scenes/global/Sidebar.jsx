400c6e053f7af077bbd2c5d62b487a220017d1af
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-04-30 23:33:51 +0300

    Remove unnecessary code from the sidebar section

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -160,20 +160,6 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Admin Tools
-            </Typography>
-            <Item
-              title="Profile Form"
-              to="/profileForm"
-              icon={<PersonOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
           </Box>
         </Menu>
       </ProSidebar>
