874863b05a613b0f67ecbb1f34f4397128c41632
midjiro <miha.gulak@gmail.com>
2024-05-18 19:00:57 +0300

    add review comments

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,56 +1,56 @@
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import { Box, IconButton, useTheme } from "@mui/material";
-import InputBase from "@mui/material/InputBase";
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { ColorModeContext, tokens } from "../../../src/theme";
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import { Box, IconButton, useTheme } from '@mui/material';
+import InputBase from '@mui/material/InputBase';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ColorModeContext, tokens } from '../../../src/theme';
 
 const Topbar = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const colorMode = useContext(ColorModeContext);
-  const navigate = useNavigate();
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+    const navigate = useNavigate();
 
-  return (
-    <Box display="flex" justifyContent="space-between" p={2}>
-      {/* SEARCH BAR */}
-      <Box
-        display="flex"
-        backgroundColor={colors.primary[400]}
-        borderRadius="3px"
-      >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-        <IconButton type="button" sx={{ p: 1 }}>
-          <SearchIcon />
-        </IconButton>
-      </Box>
+    return (
+        <Box display="flex" justifyContent="space-between" p={2}>
+            {/* SEARCH BAR */}
+            <Box
+                display="flex"
+                backgroundColor={colors.primary[400]}
+                borderRadius="3px"
+            >
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+                <IconButton type="button" sx={{ p: 1 }}>
+                    <SearchIcon />
+                </IconButton>
+            </Box>
 
-      {/* ICONS */}
-      <Box display="flex">
-        <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlinedIcon />
-          ) : (
-            <LightModeOutlinedIcon />
-          )}
-        </IconButton>
-        <IconButton>
-          <NotificationsOutlinedIcon />
-        </IconButton>
-        <IconButton onClick={() => navigate("/profileForm")}>
-          <SettingsOutlinedIcon />
-        </IconButton>
-        <IconButton onClick={() => navigate("/profile")}>
-          <PersonOutlinedIcon />
-        </IconButton>
-      </Box>
-    </Box>
-  );
+            {/* ICONS */}
+            <Box display="flex">
+                <IconButton onClick={colorMode.toggleColorMode}>
+                    {theme.palette.mode === 'dark' ? (
+                        <DarkModeOutlinedIcon />
+                    ) : (
+                        <LightModeOutlinedIcon />
+                    )}
+                </IconButton>
+                <IconButton>
+                    <NotificationsOutlinedIcon />
+                </IconButton>
+                <IconButton onClick={() => navigate('/profileForm')}>
+                    <SettingsOutlinedIcon />
+                </IconButton>
+                <IconButton onClick={() => navigate('/profile')}>
+                    <PersonOutlinedIcon />
+                </IconButton>
+            </Box>
+        </Box>
+    );
 };
 
 export default Topbar;
--- src/utils/dataProcessing.js
+++ src/utils/dataProcessing.js
@@ -1,7 +1,9 @@
+// TODO: Move fetching outside of utils in service or requests folder
+
 export async function fetchData(url, key) {
-  const response = await fetch(
-    `https://personal-finance-tracker-server.azurewebsites.net/api/${url}`
-  );
-  const data = await response.json();
-  return data[key];
+    const response = await fetch(
+        `https://personal-finance-tracker-server.azurewebsites.net/api/${url}`
+    );
+    const data = await response.json();
+    return data[key];
 }
