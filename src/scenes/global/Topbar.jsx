149573af3ce353293f3a77e8a967ee1d19cd6115
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-05-31 16:45:38 +0300

    feature: Add a popup menu when clicking on the notification bell

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -5,9 +5,16 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import { Box, IconButton, useTheme } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  useTheme,
+} from "@mui/material";
 import InputBase from "@mui/material/InputBase";
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ColorModeContext, tokens } from "../../../src/theme";
 import { UserContext } from "../../contexts";
@@ -19,6 +26,8 @@ function Topbar({ isAuthorized }) {
   const { setIsAuthorized } = useContext(UserContext);
   const isAuthorizedProperty = isAuthorized;
   const navigate = useNavigate();
+  const [anchorElement, setAnchorElement] = useState(null);
+  const open = Boolean(anchorElement);
 
   function logout() {
     localStorage.removeItem("user");
@@ -26,6 +35,14 @@ function Topbar({ isAuthorized }) {
     setIsAuthorized(false);
     navigate("/");
   }
+
+  const handleClick = (event) => {
+    setAnchorElement(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorElement(null);
+  };
 
   return (
     <>
@@ -45,25 +62,54 @@ function Topbar({ isAuthorized }) {
 
           {/* ICONS */}
           <Box display="flex">
-            <IconButton onClick={colorMode.toggleColorMode}>
-              {theme.palette.mode === "dark" ? (
-                <DarkModeOutlinedIcon />
-              ) : (
-                <LightModeOutlinedIcon />
-              )}
-            </IconButton>
-            <IconButton>
-              <NotificationsOutlinedIcon />
-            </IconButton>
-            <IconButton onClick={() => navigate("/profileForm")}>
-              <SettingsOutlinedIcon />
-            </IconButton>
-            <IconButton onClick={() => navigate("/profile")}>
-              <PersonOutlinedIcon />
-            </IconButton>
-            <IconButton onClick={() => logout()}>
-              <LogoutOutlined />
-            </IconButton>
+            <Stack direction="row" spacing={1}>
+              <IconButton onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode === "dark" ? (
+                  <DarkModeOutlinedIcon />
+                ) : (
+                  <LightModeOutlinedIcon />
+                )}
+              </IconButton>
+              <IconButton
+                id="notification-button"
+                onClick={handleClick}
+                aria-controls={open ? "notification-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <NotificationsOutlinedIcon />
+              </IconButton>
+              <IconButton onClick={() => navigate("/profileForm")}>
+                <SettingsOutlinedIcon />
+              </IconButton>
+              <IconButton onClick={() => navigate("/profile")}>
+                <PersonOutlinedIcon />
+              </IconButton>
+              <IconButton onClick={() => logout()}>
+                <LogoutOutlined />
+              </IconButton>
+            </Stack>
+            <Menu
+              id="notification-menu"
+              anchorEl={anchorElement}
+              open={open}
+              MenuListProps={{
+                "aria-labelledby": "notification-button",
+              }}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+            >
+              <MenuItem onClick={handleClose}>Notification 1</MenuItem>
+              <MenuItem onClick={handleClose}>Notification 2</MenuItem>
+              <MenuItem onClick={handleClose}>Notification 3</MenuItem>
+            </Menu>
           </Box>
         </Box>
       )}
