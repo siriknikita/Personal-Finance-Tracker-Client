91989dc65377ca1773650898b7f1886cd773c419
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-06-05 17:57:40 +0300

    chore: Update API endpoints to use HTTPS

--- src/services/dataProcessing.js
+++ src/services/dataProcessing.js
@@ -1,29 +1,35 @@
 async function fetchData(url, key) {
-  const response = await fetch(`/api/${url}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-store, no-cache",
-    },
-    mode: "cors",
-    credentials: "include",
-  });
+  const response = await fetch(
+    `https://personal-finance-tracker-server.azurewebsites.net/api/${url}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store, no-cache",
+      },
+      mode: "cors",
+      credentials: "include",
+    }
+  );
 
   const returnedData = await response.json();
   return returnedData[key];
 }
 
 async function sendPostData(url, data, key) {
-  const response = await fetch(`/api/${url}`, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-store, no-cache",
-    },
-    mode: "cors",
-    credentials: "include",
-  });
+  const response = await fetch(
+    `https://personal-finance-tracker-server.azurewebsites.net/api/${url}`,
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store, no-cache",
+      },
+      mode: "cors",
+      credentials: "include",
+    }
+  );
 
   const returnedData = await response.json();
   return returnedData[key];
